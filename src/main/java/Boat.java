75f06111c1e5c416c0bb45e009f718ca8a1a08b2
adnan <adnanmobeen444@gmail.com>
2023-01-21 15:28:38 +0530

    whole

---
+++ src/main/java/Boat.java
@@ -0,0 +1,16 @@
+import com.driver.WaterVehicle;
+
+public class Boat implements WaterVehicle {
+    private String name;
+    private int capacity;
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
